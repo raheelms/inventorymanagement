ed866132fb01d45d48eb2e641936e379ccf7b22e
raheelms <rfdaraz@gmail.com>
2025-02-16 13:10:38 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Filament\Pages\Dashboard;
 use Illuminate\Support\ServiceProvider;
+use Z3d0X\FilamentFabricator\Forms\Components\PageBuilder;
+use Z3d0X\FilamentFabricator\Resources\PageResource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        PageBuilder::configureUsing(function (PageBuilder $builder) {
+            $builder->collapsible();
+            $builder->collapsed();            
+        });
+
+        if (class_exists(PageResource::class)) {
+            PageResource::navigationGroup('Settings');
+            PageResource::navigationSort(1);
+            PageResource::navigationIcon('heroicon-o-cube');
+        }
     }
 }
